03f6bdb3badfe0b25ea00c14354eba8201ed49a3
Yifan Zhou <yfzhou.cs@gmail.com>
2023-04-09 11:45:03 -0700

    3 Stages; dalle; mae

---
+++ train4_dalle_2_obj_3_stage.sh
@@ -0,0 +1,11 @@
+python main_mujoco_robot_vision_embed_3_stage_dalle_2_obj.py \
+    --data_source_root '/data/Documents/yzhou298/dataset/extended_modattn/might_be_wrong/put_left_right_to' \
+    --data_target_root '/home/local/ASUAD/yzhou298/Documents/dataset/extended_modattn/mae_embed/put_left_right_to/' \
+    --train_set_path 'train' \
+    --val_set_path 'val' \
+    --ckpt_path '/home/local/ASUAD/yzhou298/Documents/ckpts/extended_trial' \
+    --save_ckpt \
+    --supervised_attn \
+    --curriculum_learning \
+    --ckpt None \
+    --mae_root '/home/local/ASUAD/yzhou298/github/mae'
