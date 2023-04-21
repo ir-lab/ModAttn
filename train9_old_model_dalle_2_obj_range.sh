50d0b7866629f5735f88e4f6420b5872e01d174a
Yifan Zhou <yfzhou.cs@gmail.com>
2023-04-20 19:14:05 -0700

    Added: range attn for 2 targets

---
+++ train9_old_model_dalle_2_obj_range.sh
@@ -0,0 +1,9 @@
+export CUDA_VISIBLE_DEVICES=0
+python main_mujoco_robot_extended_2_obj_range.py \
+    --data_source_root '/data/Documents/yzhou298/dataset/extended_modattn/might_be_wrong/put_left_right_to' \
+    --train_set_path 'train' \
+    --val_set_path 'val' \
+    --ckpt_path '/home/local/ASUAD/yzhou298/Documents/ckpts/dalle_3_obj_put_left_right_to' \
+    --save_ckpt \
+    --ckpt None \
+    --name extended-2-obj-dalle-range
