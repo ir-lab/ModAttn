b875c203d618815f2485143dc8d5714181ae8fb4
Yifan Zhou <yfzhou.cs@gmail.com>
2023-04-25 23:46:32 +0000

    Added: Ablation study for 2 objs

---
+++ train9_old_model_dalle_2_obj_range_no_hier_attn.sh
@@ -0,0 +1,9 @@
+export CUDA_VISIBLE_DEVICES=1
+python main_mujoco_robot_extended_2_obj_range_no_hier_mod.py \
+    --data_source_root '/home/yzhou298/data/part3/put_left_right_to_40_fovy' \
+    --train_set_path 'train' \
+    --val_set_path 'val' \
+    --ckpt_path '/home/yzhou298/data/ckpts/put_left_right_to_40_fovy' \
+    --save_ckpt \
+    --ckpt None \
+    --name extended-2-obj-dalle-range-no-hier-mod
